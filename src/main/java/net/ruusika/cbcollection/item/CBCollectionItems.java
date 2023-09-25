57450235c1d198237b7b4c249d09c7b7cb4861e1
JR1811 <janreichelt77@gmail.com>
2023-09-26 00:42:16 +0200

    suppressed necessary warnings

--- src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
+++ src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
@@ -1,15 +1,18 @@
 package net.ruusika.cbcollection.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BookItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.ruusika.cbcollection.CBCollection;
 import net.ruusika.cbcollection.util.LoggerUtilities;
 
+@SuppressWarnings({"unused", "SameParameterValue"})
 public class CBCollectionItems {
     private static final Item DELIGHT_COOKBOOK = registerItem("delight_cookbook",
-            new BookItem(new Item.Settings() /*FabricItemSettings().maxCount(1).group(ItemGroup.MISC)*/));
+            new BookItem(new FabricItemSettings().maxCount(1).group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(CBCollection.MODID, name), item);
