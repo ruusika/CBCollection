4186784549587821f924525429012386c851ee12
JR1811 <janreichelt77@gmail.com>
2023-09-26 01:37:02 +0200

    re-implemented custom Patchouli BookItem

--- src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
+++ src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
@@ -1,12 +1,12 @@
 package net.ruusika.cbcollection.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BookItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.ruusika.cbcollection.CBCollection;
+import net.ruusika.cbcollection.item.custom.BookItem;
 import net.ruusika.cbcollection.util.LoggerUtilities;
 
 @SuppressWarnings({"unused", "SameParameterValue"})
@@ -14,11 +14,11 @@ public class CBCollectionItems {
     private static final Item DELIGHT_COOKBOOK = registerItem("delight_cookbook",
             new BookItem(new FabricItemSettings().maxCount(1).group(ItemGroup.MISC)));
 
-    private static Item registerItem(String name, Item item){
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CBCollection.MODID, name), item);
     }
 
-    public static void register(){
+    public static void register() {
         LoggerUtilities.devLogger("Initialized " + CBCollection.MODID + " items");
     }
 }
---
+++ src/main/java/net/ruusika/cbcollection/item/custom/BookItem.java
@@ -0,0 +1,33 @@
+package net.ruusika.cbcollection.item.custom;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import vazkii.patchouli.api.PatchouliAPI;
+import vazkii.patchouli.common.base.PatchouliSounds;
+import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.common.book.BookRegistry;
+
+public class BookItem extends Item {
+    public BookItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if(user instanceof ServerPlayerEntity serverPlayer){
+            Identifier test = Registry.ITEM.getId(this);
+            Book book = BookRegistry.INSTANCE.books.get(test);
+            PatchouliAPI.get().openBookGUI(serverPlayer, book.id);
+            serverPlayer.playSound(PatchouliSounds.getSound(book.openSound, PatchouliSounds.BOOK_OPEN), 1, 1);
+        }
+
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+}
