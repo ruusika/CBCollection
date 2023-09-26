047a2e12b0096bb5376b6fcfa51ce133067cab79
JR1811 <janreichelt77@gmail.com>
2023-09-26 07:31:18 +0200

    changed namespaces in patchouli assets

--- src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
+++ src/main/java/net/ruusika/cbcollection/item/CBCollectionItems.java
@@ -2,7 +2,6 @@ package net.ruusika.cbcollection.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.ruusika.cbcollection.CBCollection;
